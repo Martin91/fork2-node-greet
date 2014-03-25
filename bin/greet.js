c63390ca26bc3fc6996aa7f094466303d9f869be
Martin <hongzeqin@gmail.com>
2014-03-25 10:08:20 +0800

    use minimist to parse arguments from command line

--- bin/greet.js
+++ bin/greet.js
@@ -3,6 +3,7 @@
 // require folders as modules
 // http://nodejs.org/api/modules.html#modules_folders_as_modules
 greet = require('greet');
+var parseArgs = require('minimist');
 
-name = process.argv[2];
-console.log(greet(name));
+var argv = parseArgs(process.argv);
+console.log(greet(argv._[2], argv.drunk));
